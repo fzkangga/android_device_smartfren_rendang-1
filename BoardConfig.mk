a22d7fb77e3d8110960b8851251c50cd630464f1
Ethan Chen <intervigil@gmail.com>
2016-03-15 17:34:19 -0700

    rendang: Don't use AOSP HAL
    Change-Id: I8e3b7883340f0b542d64a592f6f512fd2ef41335

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,14 +36,9 @@ endif
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
 
 # Camera
-BOARD_CAMERA_SENSORS := \
-    ov5670_q5v41b \
-    ov5648_q5v22e_rendang \
-    gc2355_rendang
-
+BOARD_CAMERA_SENSORS := ov5670_q5v41b ov5648_q5v22e_rendang gc2355_rendang
 TARGET_USE_VENDOR_CAMERA_EXT := true
 USE_DEVICE_SPECIFIC_CAMERA := true
-TARGET_USES_AOSP := true
 
 # DPM NSRM Feature
 TARGET_LDPRELOAD := libNimsWrap.so
