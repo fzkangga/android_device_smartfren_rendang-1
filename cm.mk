60bb3b78d320c61a5baed980f8f8683f27b7ef5d
Ethan Chen <intervigil@gmail.com>
2016-05-26 14:10:08 -0700

    rendang: Update to match msm8916-common
    * msm8916-common now supports msm8939 as well, which required some
      changes. Track those changes here.
    
    Change-Id: If3644f9a9b80c412fab12978048bc7badabce78c

--- cm.mk
+++ cm.mk
@@ -17,6 +17,9 @@ $(call inherit-product, device/smartfren/rendang/full_rendang.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
+# Must define platform variant before including any common things
+TARGET_BOARD_PLATFORM_VARIANT := msm8916
+
 PRODUCT_NAME := cm_rendang
 BOARD_VENDOR := smartfren
 PRODUCT_DEVICE := rendang
