2eca0575d6cd3621551049e5b7dcf407d838e7ff
LuK1337 <priv.luk@gmail.com>
2017-09-05 19:53:26 +0200

    rendang: Build our MultiHAL sensors HAL
    Change-Id: I32172a0a6f5b3bf15ead878e91c2edd25b6f1625

--- device.mk
+++ device.mk
@@ -114,7 +114,11 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     calmodule.cfg \
     libcalmodule_memsic \
-    sensors.msm8916
+    sensors.msm8916 \
+    sensors.rendang
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/_hals.conf:system/vendor/etc/sensors/_hals.conf
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/spn-conf.xml:system/etc/spn-conf.xml
