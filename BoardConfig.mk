de7ac740e7ef136fbb29c48c4d760ae872f25072
fzkangga <titahpriyangga@gmail.com>
2020-09-24 15:35:11 +0700

    rendang: Move dexopt flags to device

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,6 +30,16 @@ BOARD_CAMERA_SENSORS := ov5670_q5v41b ov5648_q5v22e_rendang gc2355_rendang
 TARGET_USE_VENDOR_CAMERA_EXT := true
 USE_DEVICE_SPECIFIC_CAMERA := true
 
+# Dex
+ifeq ($(HOST_OS),linux)
+    ifeq ($(TARGET_BUILD_VARIANT),user)
+        ifeq ($(WITH_DEXPREOPT),)
+            WITH_DEXPREOPT := true
+            WITH_DEXPREOPT_BOOT_IMG_ONLY ?= true
+        endif
+    endif
+endif
+
 # Flags
 BOARD_NO_SECURE_DISCARD := true
 
